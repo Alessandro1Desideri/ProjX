b79c9b6ff385107aefe1fac13e66e3e104a1ceb6
Alessandro1Desideri <alessandro1desideri@gmail.com>
2022-09-15 15:01:24 +0200

    Added React Hooks

--- react-login/src/login.js
+++ react-login/src/login.js
@@ -1,4 +1,22 @@
+import { useRef, useState, useEffect} from 'react';
+
 const Login = () => {
+    const useRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSucces] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user,pwd])
+
     return (
         <div>
 
